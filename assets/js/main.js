53a510325949bdd4bf7da29d5de19a092d4e41cb
Iain Rosen <iain.rosen@gmail.com>
2019-11-14 11:55:53 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,6 @@
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
-            document.getElementById("preload-msg").style.visibility = "hidden";
 		});
 
 	// Mobile?
